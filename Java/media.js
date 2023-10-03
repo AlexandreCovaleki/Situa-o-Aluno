f49800d6d4fe71e3c7ca1455d7ddf9a71753bb1a
AlexandreCovaleki <113298904+AlexandreCovaleki@users.noreply.github.com>
2023-10-03 20:17:08 -0300

    Add files via upload

--- Java/media.js
+++ Java/media.js
@@ -1,24 +1,27 @@
+alert("testando")
+//Cria referência ao form e elementos de resposta do programa
 const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
-
-form.addEventListener("submit",(e)) =>{
+ 
+ //cria um ouvinte
+  form.addEventListener("submit", (e) =>{
     e.preventDefault()
-
     const nome = form.inNome.value
-    const nota1 = Number(form.inNota1.value)
-    const nota2 = Number(form.inNota2.value)
-    const media =(nota1 + nota2)/2
-    resp1.innerText = `Media das Notas ${media.toFixed(2)}`
+    const nota1 = number(form.innotal.value)
+    const nota2 = number(form.innota2.value)
 
-    if(media >= 7) {
-        resp2.innerText = `Parabens ${nome}! Voce foi aprovado(a)`
-        resp2.style.color = "blue" 
-    } else if(media >=4) {
-        resp2.innerText = `Atenção ${nome}! Voce está de exame`
-        resp2.style.color = "green"
-    } else{
-        resp2.innerText = `Ops ${nome}! Voce foi reprovado(a)`
-        resp2.style.color = "red"
+    const media = (nota1 + nota2)/2
+    resp1.innerText = 'media das notas $ {media.tofixed(2))}'
+
+    if(media >=7){
+      resp2;innerText = 'paraben $(adolescente}! você foi aprovado(a)'
+      resp2. style.color = "blue"
+    }else if (media >=4) {
+      resp2.innerText = 'atenção você esta de exame'
+      resp2.style.color = "green"
+    }else{
+      resp2.innerText = 'ops ${aborrescente}!você foi reprovado(a)'
+      resp2.style.color = 'red'
     }
-}
+  })
