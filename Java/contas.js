b1fb31b24544386e47a1f2f5b9972c1f41c12674
AlexandreCovaleki <113298904+AlexandreCovaleki@users.noreply.github.com>
2023-10-30 08:19:24 -0300

    Add files via upload

---
+++ Java/contas.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+let numContas = 0
+let ValTotal = 0
+let resposta = ""
+
+removeEventListener.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const descricao = frm.inDescrição.value
+    const valor = Numbe(rm.inValor.value)
+
+    numContas
+})
